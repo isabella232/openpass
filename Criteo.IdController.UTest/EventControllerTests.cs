fc1c2906fba0a5c4010d885a3ea25507db1cc001
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2020-08-28 17:44:38 +0200

    [Id-Controller] Add missing tests (UID priority for UA)
    Change-Id: Ib4f37333ee20ef41fbb3816370d25e80f2f42f24
    JIRA:IDPS-439

--- Criteo.IdController.UTest/EventControllerTests.cs
+++ Criteo.IdController.UTest/EventControllerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using Moq;
 using NUnit.Framework;
@@ -16,6 +17,11 @@ namespace Criteo.IdController.UTest
         private EventController _eventController;
         private Mock<IGlupService> _glupServiceMock;
         private Mock<IAgentSource> _agentSourceMock;
+
+        private const string _testingLwid = "00000000-0000-0000-0000-000000000001";
+        private const string _testingUid = "00000000-0000-0000-0000-000000000002";
+        private const string _testingIfa = "00000000-0000-0000-0000-000000000003";
+
 
         [SetUp]
         public void Setup()
@@ -45,6 +51,26 @@ namespace Criteo.IdController.UTest
             _glupServiceMock.Verify(
                 x => x.Emit(It.IsAny<IdControllerGlup>()),
                 Times.Never);
+        }
+
+        [TestCase(null, null, null, null)]
+        [TestCase("invalidUid", null, null, null)]
+        [TestCase(_testingLwid, null, null, _testingLwid)]
+        [TestCase(null, _testingUid, null, _testingUid)]
+        [TestCase(null, null, _testingIfa, _testingIfa)]
+        [TestCase(_testingLwid, _testingUid, null, _testingUid)]
+        [TestCase(_testingLwid, null, _testingIfa, _testingIfa)]
+        [TestCase(null, _testingUid, _testingIfa, _testingIfa)]
+        [TestCase(_testingLwid, _testingUid, _testingIfa, _testingIfa)]
+        public void UserAgentParsingReceivesExpectedUid(string localwebid, string uid, string ifa, string expectedUid)
+        {
+            // This test also checks that the Guid is properly parsed if exists
+            _eventController.SaveEvent(EventType.BannerRequest, "originHost.com", localwebid, uid, ifa);
+
+            var parsedExpectedUid = !string.IsNullOrEmpty(expectedUid) ? Guid.Parse(expectedUid) : (Guid?) null;
+            _agentSourceMock.Verify(
+                x => x.Get(It.IsAny<AgentKey>(), It.Is<Guid?>(g => g.Equals(parsedExpectedUid))),
+                Times.Once);
         }
     }
 }
