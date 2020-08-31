2ee4e0dfdd75c4bc1c0e2193064997f4b42a9d57
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2020-08-31 18:17:39 +0200

    [Id-Controller] Emit events for initial banner and learn more
    This commit adds a utils with useful functions, specially to emit an event.
    It also sends events from front-end for initial banner and learn more.
    Options will be done in another review to keep it simple.
    
    Change-Id: Ibfdfca3b443e480917ac5b5c18f16a1b02d4eefc
    JIRA:IDPS-439

---
+++ Criteo.IdController/Javascript/src/utils.ts
@@ -0,0 +1,50 @@
+﻿// Handy functions used by static sites
+
+enum EventType {
+    Unknown = 0,
+    BannerRequest = 1,
+    ConsentGranted = 2,
+    ConsentNotGranted = 3,
+    BannerIgnored = 4,
+    LearnMore = 5,
+    EmailShared = 6
+}
+
+class Utils {
+    private static readonly eventUrl = "https://id-controller.crto.in/api/event"; // Use http://localhost:1234/api/event for development
+
+    static eventType = EventType;
+
+    static getCurrentHostname(window: Window): string {
+        return window.location.hostname;
+    }
+
+    static sendEvent(eventType: number, originHost: string): void {
+        const targetUrl = this.generateTargetUrl(eventType, originHost);
+        // TODO: Get localwebid, uid or ifa from cookies (or tags)
+
+        fetch(targetUrl, {
+            method: "POST",
+            credentials: "include",
+        })
+        .catch((error) => console.error(`[IdController] Error when sending event: ${error}`));
+    }
+
+    private static generateTargetUrl(
+        eventType: number,
+        originHost: string,
+        localwebid?: string,
+        uid?: string,
+        ifa?: string) : string {
+        let targetUrl = `${this.eventUrl}?eventType=${eventType}&originHost=${originHost}`;
+
+        if (localwebid)
+            targetUrl += `&localwebid=${localwebid}`;
+        if (uid)
+            targetUrl += `&uid=${uid}`;
+        if (ifa)
+            targetUrl += `&ifa=${ifa}`;
+
+        return targetUrl;
+    }
+}
