068c57b194152ca36227a2d927fc75e99c8d4d7a
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2020-09-01 17:34:05 +0200

    [Id-Controller] Inject CSS inline and unify in one file
    Now the CSS is contained in one centralized file as most of the style
    is shared by all views. Also injected inline if required by using the
    <style inline src="path/to/css"></style> directive. Similar to JS.
    
    Change-Id: Ic6927363272722a8f94266ca4541b06fef011f9d

--- Criteo.IdController/TagHelpers/InlineTagHelper.cs
+++ Criteo.IdController/TagHelpers/InlineTagHelper.cs
@@ -1,56 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.FileProviders;
 
-// Inspired by this code: https://www.meziantou.net/inlining-a-stylesheet-a-javascript-or-an-image-file-using-a-taghelper-in-asp-net.htm
 namespace Criteo.IdController.TagHelpers
 {
-    public abstract class InlineTagHelper : TagHelper
+    // Use the word 'inline' within the script tag to inject code inline (specified in the src attribute)
+    // This class supports both (JS) scripts and (CSS) styles
+    // Note that external styles are referred by using the <link> tag but this custom approach is more readable
+
+    // Example: <script inline type="text/javascript" src="/dist/js/js-code.js"></script>
+    // Example: <style inline src="/dist/css/css-code.css"></style>
+
+    [HtmlTargetElement("script", Attributes = "inline")]
+    [HtmlTargetElement("style", Attributes = "inline")]
+    public class InlineTagHelper : InlineTagHelperBase
     {
-        private const string CacheKeyPrefix = "InlineTagHelper-";
+        [HtmlAttributeName("src")]
+        public string Src { get; set; }
 
-        private readonly IHostingEnvironment _hostingEnvironment;
-        private readonly IMemoryCache _cache;
+        public InlineTagHelper(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
+            : base(hostingEnvironment, cache)
+        { }
 
-        protected InlineTagHelper(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            _hostingEnvironment = hostingEnvironment;
-            _cache = cache;
-        }
-
-        private async Task<T> GetContentAsync<T>(ICacheEntry entry, string path, Func<IFileInfo, Task<T>> getContent)
-        {
-            var fileProvider = _hostingEnvironment.ContentRootFileProvider;
-            var changeToken = fileProvider.Watch(path);
-
-            entry.SetPriority(CacheItemPriority.NeverRemove);
-            entry.AddExpirationToken(changeToken);
-
-            var file = fileProvider.GetFileInfo(path);
-            if (file == null || !file.Exists)
-                return default(T);
-
-            return await getContent(file);
-        }
-
-        protected Task<string> GetFileContentAsync(string path)
-        {
-            return _cache.GetOrCreateAsync(CacheKeyPrefix + path, entry => GetContentAsync(entry, path, ReadFileContentAsStringAsync));
-        }
-
-        private static async Task<string> ReadFileContentAsStringAsync(IFileInfo file)
-        {
-            using (var stream = file.CreateReadStream())
-            using (var textReader = new StreamReader(stream))
+            var fileContent = await GetFileContentAsync(Src);
+            if (fileContent == null)
             {
-                return await textReader.ReadToEndAsync();
+                output.SuppressOutput();
+                return;
             }
+
+            output.Attributes.RemoveAll("inline");
+            output.Attributes.RemoveAll("src");
+            output.Content.AppendHtml(fileContent);
         }
     }
 }
---
+++ Criteo.IdController/TagHelpers/InlineTagHelperBase.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.FileProviders;
+
+// Inspired by this code: https://www.meziantou.net/inlining-a-stylesheet-a-javascript-or-an-image-file-using-a-taghelper-in-asp-net.htm
+namespace Criteo.IdController.TagHelpers
+{
+    public abstract class InlineTagHelperBase : TagHelper
+    {
+        private const string CacheKeyPrefix = "InlineTagHelper-";
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IMemoryCache _cache;
+
+        protected InlineTagHelperBase(IHostingEnvironment hostingEnvironment, IMemoryCache cache)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _cache = cache;
+        }
+
+        private async Task<T> GetContentAsync<T>(ICacheEntry entry, string path, Func<IFileInfo, Task<T>> getContent)
+        {
+            var fileProvider = _hostingEnvironment.ContentRootFileProvider;
+            var changeToken = fileProvider.Watch(path);
+
+            entry.SetPriority(CacheItemPriority.NeverRemove);
+            entry.AddExpirationToken(changeToken);
+
+            var file = fileProvider.GetFileInfo(path);
+            if (file == null || !file.Exists)
+                return default(T);
+
+            return await getContent(file);
+        }
+
+        protected Task<string> GetFileContentAsync(string path)
+        {
+            return _cache.GetOrCreateAsync(CacheKeyPrefix + path, entry => GetContentAsync(entry, path, ReadFileContentAsStringAsync));
+        }
+
+        private static async Task<string> ReadFileContentAsStringAsync(IFileInfo file)
+        {
+            using (var stream = file.CreateReadStream())
+            using (var textReader = new StreamReader(stream))
+            {
+                return await textReader.ReadToEndAsync();
+            }
+        }
+    }
+}
