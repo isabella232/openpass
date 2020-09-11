e8af7ece8d8a8d0f2697f89ca6867df577b3c6fa
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2020-09-11 15:16:16 +0200

    [IdController] Add public API to retrieve IFA and create lib
    Create window.idcapi function that is a promise that returns the IFA in cookies.
    Tests added as well.
    
    Example: window.__idcapi().then(ifa => console.log(ifa));
    
    Change-Id: I7e54dca7fdb6cb940344e03f5575ed2e6d567e4c
    JIRA:IDPS-449

--- Criteo.IdController/Javascript/src/widget.ts
+++ Criteo.IdController/Javascript/src/widget.ts
@@ -1,85 +1,9 @@
-﻿// IdControllerWidget class must be initialized from the front-end
-// example: const widget = new IdControllerWidget();
+﻿import IdController from './IdController';
 
-class IdControllerWidget {
-    private readonly widgetUrl = 'https://id-controller.crto.in/api/widget';
-
-    //Use this URL for local testing
-    //private readonly widgetUrl = 'http://localhost:1234/api/widget';
-
-    private readonly parent!: HTMLElement;
-    private readonly height!: number;
-    private readonly width!: number;
-    private readonly originHost!: string;
-    private readonly expiryHours = 13 * 24 * 30;
-    private readonly ifaFirstPartyCookieName = "id_controller_ifa";
-
-    constructor(parentId: string, height: number = 250, width: number = 500) {
-        const parent = document.getElementById(parentId);
-        if (!parent) {
-            console.error("[IdControllerWidget] Parent not found.");
-            return;
-        }
-
-        this.parent = parent;
-        this.height = height;
-        this.width = width;
-        this.originHost = this.getCurrentHostname();
-
-        this.createAndAppendIframe();
-        this.addSetCookieEventListener();
-    }
-
-    private getCurrentHostname(): string {
-        return window.location.hostname;
-    }
-
-    private createAndAppendIframe(): void {
-        const iframe = document.createElement("iframe");
-        iframe.src = `${this.widgetUrl}?originHost=${this.originHost}`;
-        iframe.height = this.height.toString();
-        iframe.width = this.width.toString();
-        iframe.style.border = "none";
-
-        this.parent.appendChild(iframe);
-    }
-
-    private static setCookieString(
-        key: string,
-        value: string,
-        expires: string,
-        domain: string | undefined,
-        doc: Document,
-    ) {
-        let cookie = key + "=" + encodeURIComponent(value) + ";" + expires + ";";
-        if (domain && domain !== "") {
-            cookie += "domain=." + domain + ";";
-        }
-        doc.cookie = cookie + "path=/";
-    }
-
-
-    private addSetCookieEventListener(): void {
-
-        const evtListener = (event: MessageEvent) => {
-            const data = event.data;
-
-            // Discard messages coming from other iframes
-            if (!data || !data.isIdControllerMessage) {
-                return;
-            }
-
-            // Prevent the message to propagate to other listeners
-            event.stopImmediatePropagation();
-
-            if (data.ifa) {
-                const date = new Date();
-                date.setTime(date.getTime() + (this.expiryHours * 60 * 60 * 1000));
-                const expires = "expires=" + date.toUTCString();
-                IdControllerWidget.setCookieString(this.ifaFirstPartyCookieName, data.ifa, expires, this.originHost, document);
-            }
-        }
-
-        window.addEventListener("message", evtListener, true);
+declare global {
+    interface Window {
+        __idcapi(): Promise<string | undefined>;
     }
 }
+
+window.__idcapi = IdController.getIfa;
---
+++ Criteo.IdController/Javascript/test/IdController.spec.ts
@@ -0,0 +1,35 @@
+﻿// Use require to inject the code (cannot use import because it's not a TS module)
+// This is a workaround so we can test that the IdController API works as expected
+import IdController from '../src/IdController';
+
+const cookieName = 'id_controller_ifa';
+
+describe('IdController API', () => {
+    let spyCookie: jest.SpyInstance<string, any>;
+
+    let mockedCookie: string;
+    const mockIfaCookie = (value: string | undefined): void => {
+        mockedCookie = value ? `${cookieName}=${value}` : '';
+    }
+
+    beforeEach(() => {
+        spyCookie = jest.spyOn(document, 'cookie', 'get').mockImplementation(() => mockedCookie);
+    });
+
+    afterEach(() => {
+        mockIfaCookie(undefined);
+        spyCookie.mockClear();
+    });
+
+    test('returns IFA if exists', async () => {
+        const ifa = 'mockedIFA';
+        mockIfaCookie(ifa);
+        const returnedIfa = await IdController.getIfa();
+        expect(returnedIfa).toEqual(ifa);
+    });
+
+    test('returns undefined when IFA does not exist', async () => {
+        const returnedIfa = await IdController.getIfa();
+        expect(returnedIfa).toEqual(undefined);
+    });
+});
