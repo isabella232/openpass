9b9a02e9e371b70cd3c3d8ea4f3abab2a07b72d4
Dmytro <d.kovtun@criteo.com>
2021-01-26 16:09:36 +0200

    [SSO] Add presubmit scripts
    Add scripts
    disable analytics
    
    Change-Id: I0ff92586706c6991b078b04674b44bf6889db700

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -1,9 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+WORKSPACE_PATH=`cygpath -u "${WORKSPACE}"`
+REVIEW_MESSAGE_FILE="${WORKSPACE_PATH}/REVIEW_MESSAGE"
 
+executelogandtime() {
+  COMMAND=$1
+  TEXT=$2
+  START=$(date +%s)
+  eval $COMMAND
+  RESULT=$?
+  END=$(date +%s)
+  TIME=$(( $END - $START ))
+  MINUTES=$(( $TIME / 60 ))
+  SECONDS=$(( $TIME % 60 ))
+
+  if [ $RESULT -eq 0 ] ; then
+    echo "$TEXT succeeded in ${MINUTES}m${SECONDS}s." >> $REVIEW_MESSAGE_FILE
+    return $RESULT
+  else
+    echo "$TEXT failed in ${MINUTES}m${SECONDS}s." >> $REVIEW_MESSAGE_FILE
+    exit $RESULT
+  fi
+}
+
+BASE_DIR=`dirname $0`
 if [ "$1" = "end" ]; then
-    # Run Javascript UTests
-    pushd ./Criteo.IdController/Javascript
-    npm install
-    npm test
-    popd
+    cd $BASE_DIR/../id-controller-ui || return
+    UI_CHANGE_COUNT=`git diff-tree --no-commit-id --name-only -r HEAD | grep id-controller-ui | wc -l`
+
+    if [ $UI_CHANGE_COUNT -ne 0 ]; then
+        executelogandtime 'npm run lint' 'Angular app lint'
+        executelogandtime 'npm run test' 'Angular app test'
+    else
+        echo "No change in UI source code, integration tests will not be triggered."
+    fi
 fi
