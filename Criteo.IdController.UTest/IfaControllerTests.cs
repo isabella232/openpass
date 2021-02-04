cff4c46d97f3d5de386a9cc7fc3bd45a087e1bec
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2021-02-04 17:33:33 +0100

    Remove ITests and migrate IfaControllerTests to UTests
    ITests are currently broken but not necessarily needed. Migrating to UTests.
    
    Change-Id: I3c61aed2bc0509aa476cfcdaf246caf04e5b42b9

---
+++ Criteo.IdController.UTest/IfaControllerTests.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Criteo.IdController.Controllers;
+using Criteo.IdController.Helpers;
+using Metrics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Criteo.IdController.UTest
+{
+    [TestFixture]
+    public class IfaControllerTests
+    {
+        private Mock<IUserManagementHelper> _userManagementHelperMock;
+        private Mock<IMetricsRegistry> _metricRegistryMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userManagementHelperMock = new Mock<IUserManagementHelper>();
+            _metricRegistryMock = new Mock<IMetricsRegistry>();
+            _metricRegistryMock.Setup(mr => mr.GetOrRegister(It.IsAny<string>(), It.IsAny<Func<Counter>>())).Returns(new Counter(Granularity.CoarseGrain));
+        }
+
+        [Test]
+        public void TestCreateIfaWithoutPii()
+        {
+            var ifaController = GetIfaController();
+
+            var response = ifaController.GetOrCreateIfa(null);
+
+            // Returned IFA
+            var data = GetResponseData(response);
+            Assert.NotNull(data);
+
+            // IFA in cookie
+            var cookie = GetSetCookieHeaderIfa(ifaController.HttpContext.Response);
+            Assert.NotNull(cookie);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("pii")] // valid
+        public void TestCreateIfaWithValidOrInvalidPii(string pii)
+        {
+            var ifaController = GetIfaController();
+
+            var response = ifaController.GetOrCreateIfa(pii);
+
+            // Returned IFA
+            var data = GetResponseData(response);
+            Assert.NotNull(data);
+
+            // IFA in cookie
+            var cookie = GetSetCookieHeaderIfa(ifaController.HttpContext.Response);
+            Assert.NotNull(cookie);
+        }
+
+        [Test]
+        public void TestGetIfaFromCookie()
+        {
+            var ifaUserSide = "62BB805D-9E63-4FE0-AB7D-4B514F86D63C";
+
+            var cookies = new Dictionary<string, string> { { "ifa", ifaUserSide } };
+            var ifaController = GetIfaController(cookies);
+
+            var response = ifaController.GetOrCreateIfa(null);
+
+            // Returned IFA
+            var data = GetResponseData(response);
+            Assert.NotNull(data);
+            StringAssert.Contains(ifaUserSide, data);
+
+            // IFA in cookie
+            var cookie = GetSetCookieHeaderIfa(ifaController.HttpContext.Response);
+            Assert.NotNull(cookie);
+            StringAssert.Contains(ifaUserSide, cookie.Value.ToString());
+        }
+
+        [Test]
+        public void TestCookieIfaIsIgnoredWhenPiiPresent()
+        {
+            var ifaUserSide = "62BB805D-9E63-4FE0-AB7D-4B514F86D63C";
+
+            var cookies = new Dictionary<string, string> { { "ifa", ifaUserSide } };
+            var ifaController = GetIfaController(cookies);
+
+            var response = ifaController.GetOrCreateIfa("pii");
+
+            // Returned IFA
+            var data = GetResponseData(response);
+            Assert.NotNull(data);
+            StringAssert.DoesNotContain(ifaUserSide, data);
+
+            // IFA in cookie
+            var cookie = GetSetCookieHeaderIfa(ifaController.HttpContext.Response);
+            Assert.NotNull(cookie);
+            StringAssert.DoesNotContain(ifaUserSide, cookie.Value.ToString());
+        }
+
+        [Test]
+        public void TestDeleteIfa()
+        {
+            var ifaUserSide = "62BB805D-9E63-4FE0-AB7D-4B514F86D63C";
+
+            var cookies = new Dictionary<string, string> { { "ifa", ifaUserSide } };
+            var ifaController = GetIfaController(cookies);
+
+            var response = ifaController.DeleteIfa();
+
+            // Returned IFA
+            var data = GetResponseData(response);
+            Assert.IsNull(data);
+
+            // IFA in cookie (set to an empty value and already expired)
+            var cookie = GetSetCookieHeaderIfa(ifaController.HttpContext.Response);
+            Assert.IsNotNull(cookie);
+            Assert.AreEqual(0, cookie.Value.Length);
+        }
+
+        #region Helpers
+        private IfaController GetIfaController(Dictionary<string, string> cookies = null)
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Cookies = new RequestCookieCollection(cookies);
+
+            var ifaController = new IfaController(_userManagementHelperMock.Object, _metricRegistryMock.Object)
+            {
+                ControllerContext = new ControllerContext { HttpContext = httpContext }
+            };
+
+            return ifaController;
+        }
+
+        private SetCookieHeaderValue GetSetCookieHeaderIfa(HttpResponse response)
+        {
+            var setCookies = response.GetTypedHeaders().SetCookie;
+            var cookie = setCookies?.FirstOrDefault(c => c.Name == "ifa");
+
+            return cookie;
+        }
+
+        private string GetResponseData(IActionResult response)
+        {
+            var responseContent = response as OkObjectResult;
+            var data = responseContent?.Value.ToString();
+
+            return data;
+        }
+        #endregion
+    }
+}
