272e516c67d1818f576a3ac03e5282205e293415
Jesús Alberto Polo García <ja.pologarcia@criteo.com>
2021-02-24 13:51:34 +0100

    Add CookieHelper to centralize interaction with cookies
    Both authenticated and non-authenticated workflows are being unified,
    this new helper centralizes the cookie interactions so code and variables
    are not duplicated and expect the same behaviour in both cases.
    
    Change-Id: I8e0a11934278d68e94d6f9e951b982cdac03a513
    JIRA:USRF-46

---
+++ Criteo.IdController.UTest/Helpers/CookieHelperTests.cs
@@ -0,0 +1,60 @@
+using System;
+using Criteo.IdController.Helpers;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
+
+namespace Criteo.IdController.UTest.Helpers
+{
+    [TestFixture]
+    public class CookieHelperTests
+    {
+        private const int _cookieLifetimeDays = 390;
+        private const string _identifierCookieName = "openpass_token";
+
+        private ICookieHelper _cookieHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cookieHelper = new CookieHelper();
+        }
+
+        #region Cookie-specific
+        [Test]
+        public void GetIdentifierCookieTest()
+        {
+            string placeholder;
+            var cookieContainerMock = new Mock<IRequestCookieCollection>();
+
+            _cookieHelper.TryGetIdentifierCookie(cookieContainerMock.Object, out _);
+
+            cookieContainerMock.Verify(c => c.TryGetValue(It.Is<string>(k => k == _identifierCookieName), out placeholder));
+        }
+
+        [Test]
+        public void SetIdentifierCookieTest()
+        {
+            var cookieContainerMock = new Mock<IResponseCookies>();
+            _cookieHelper.SetIdentifierCookie(cookieContainerMock.Object, "value");
+
+            var expectedExpire = DateTime.Today.AddDays(_cookieLifetimeDays);
+            cookieContainerMock.Verify(c =>
+                c.Append(
+                    It.Is<string>(k => k == _identifierCookieName),
+                    It.IsAny<string>(),
+                    It.Is<CookieOptions>(co => co.Expires.Value.DateTime.Equals(expectedExpire))));
+        }
+
+        [Test]
+        public void RemoveIdentifierCookieTest()
+        {
+            var cookieContainerMock = new Mock<IResponseCookies>();
+
+            _cookieHelper.RemoveIdentifierCookie(cookieContainerMock.Object);
+
+            cookieContainerMock.Verify(c => c.Delete(It.Is<string>(k => k == _identifierCookieName)));
+        }
+        #endregion
+    }
+}
---
+++ Criteo.IdController/Helpers/CookieHelper.cs
@@ -0,0 +1,51 @@
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Criteo.IdController.Helpers
+{
+    public interface ICookieHelper
+    {
+        bool TryGetIdentifierCookie(IRequestCookieCollection cookieContainer, out string value);
+        void SetIdentifierCookie(IResponseCookies cookieContainer, string value);
+        void RemoveIdentifierCookie(IResponseCookies cookieContainer);
+    }
+
+    public class CookieHelper : ICookieHelper
+    {
+        private const int _cookieLifetimeDays = 390;
+        private const string _identifierCookieName = "openpass_token";
+
+        private readonly CookieOptions _defaultCookieOptions;
+
+        public CookieHelper()
+        {
+            _defaultCookieOptions = new CookieOptions
+            {
+                Expires = new DateTimeOffset(DateTime.Today.AddDays(_cookieLifetimeDays)),
+                Secure = true
+            };
+        }
+
+        #region Generic methods
+        private bool TryGetCookie(IRequestCookieCollection cookieContainer, string name, out string value) =>
+            cookieContainer.TryGetValue(name, out value);
+
+        private void SetCookie(IResponseCookies cookieContainer, string name, string value) =>
+            cookieContainer.Append(name, value, _defaultCookieOptions);
+
+        private void RemoveCookie(IResponseCookies cookieContainer, string name) =>
+            cookieContainer.Delete(name);
+        #endregion
+
+        #region Cookie-specific methods
+        public bool TryGetIdentifierCookie(IRequestCookieCollection cookieContainer, out string value) =>
+            TryGetCookie(cookieContainer, _identifierCookieName, out value);
+
+        public void SetIdentifierCookie(IResponseCookies cookieContainer, string value) =>
+            SetCookie(cookieContainer, _identifierCookieName, value);
+
+        public void RemoveIdentifierCookie(IResponseCookies cookieContainer) =>
+            RemoveCookie(cookieContainer, _identifierCookieName);
+        #endregion
+    }
+}
