ead54f9a24e8de0cb3b20829e58269179dd04da9
Dmytro <d.kovtun@criteo.com>
2021-03-09 14:20:41 +0200

    [OpenPass] Close dialog window by itself
    Change-Id: I3d0b0659d3b0ce4e1cbb885a5aa584e0e02330c3

--- id-controller-ui/projects/id-controller-ui/src/app/containers/otp-widget/success-signed-view/success-signed-view.component.ts
+++ id-controller-ui/projects/id-controller-ui/src/app/containers/otp-widget/success-signed-view/success-signed-view.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { localStorage } from '@shared/utils/storage-decorator';
 import { timer } from 'rxjs';
 import { AuthService } from '@services/auth.service';
 import { PostMessagesService } from '@services/post-messages.service';
 import { PostMessagePayload } from '@shared/types/post-message-payload';
 import { PostMessageActions } from '@shared/enums/post-message-actions.enum';
+import { WINDOW } from '@utils/injection-tokens';
 
 @Component({
   selector: 'usrf-success-signed-view',
@@ -21,11 +22,18 @@ export class SuccessSignedViewComponent implements OnInit {
     return (this.userEmail || '***')?.slice(0, visibleCharsCount) + '*'.repeat(hiddenChars);
   }
 
-  constructor(private authService: AuthService, private postMessagesService: PostMessagesService) {}
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    private authService: AuthService,
+    private postMessagesService: PostMessagesService
+  ) {}
 
   ngOnInit() {
     const message: PostMessagePayload = { action: PostMessageActions.closeChild };
     this.authService.setTokenToOpener();
-    timer(5000).subscribe(() => this.postMessagesService.sendMessage(message));
+    timer(5000).subscribe(() => {
+      this.postMessagesService.sendMessage(message);
+      this.window.close(); // fallback
+    });
   }
 }
