57087d75a9ce2b21cec07b65eed0632bcab62887
Dmytro <d.kovtun@criteo.com>
2021-04-02 16:24:22 +0300

    [OpenPass] Add translations
    Change-Id: I69ab2fe2bb54ab299e26e719f8d8ff877fa99549
    JIRA:USRF-52

--- Criteo.IdController.UI/projects/open-pass/src/app/components/google-auth-button/google-auth-button.module.ts
+++ Criteo.IdController.UI/projects/open-pass/src/app/components/google-auth-button/google-auth-button.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoogleAuthButtonComponent } from './google-auth-button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [GoogleAuthButtonComponent],
   exports: [GoogleAuthButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
 })
 export class GoogleAuthButtonModule {}
--- Criteo.IdController.UI/projects/open-pass/src/app/containers/otp-widget/sso-view/sso-view.module.ts
+++ Criteo.IdController.UI/projects/open-pass/src/app/containers/otp-widget/sso-view/sso-view.module.ts
@@ -4,9 +4,10 @@ import { SsoViewComponent } from './sso-view.component';
 import { SsoViewRoutingModule } from './sso-view-routing.module';
 import { GoogleAuthButtonModule } from '@components/google-auth-button/google-auth-button.module';
 import { FacebookAuthButtonModule } from '@components/facebook-auth-button/facebook-auth-button.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SsoViewComponent],
-  imports: [CommonModule, SsoViewRoutingModule, GoogleAuthButtonModule, FacebookAuthButtonModule],
+  imports: [CommonModule, SsoViewRoutingModule, GoogleAuthButtonModule, FacebookAuthButtonModule, TranslateModule],
 })
 export class SsoViewModule {}
