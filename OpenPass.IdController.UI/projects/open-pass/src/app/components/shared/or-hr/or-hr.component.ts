d6b477684ade895f5a15279303938bc1b73ac680
Dmytro <d.kovtun@criteo.com>
2021-05-13 17:17:15 +0300

    [OpenPass] Extract a component
    Change-Id: Ieae2b66fcb77462f5b30eb61af218b11853dedb8
    JIRA:USRF-70

---
+++ OpenPass.IdController.UI/projects/open-pass/src/app/components/shared/or-hr/or-hr.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'usrf-or-hr',
+  templateUrl: './or-hr.component.html',
+  styleUrls: ['./or-hr.component.scss'],
+})
+export class OrHrComponent {}
--- OpenPass.IdController.UI/projects/open-pass/src/app/components/shared/shared.module.ts
+++ OpenPass.IdController.UI/projects/open-pass/src/app/components/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { OrHrComponent } from './or-hr/or-hr.component';
 
 @NgModule({
-  declarations: [CopyrightComponent],
-  exports: [CopyrightComponent],
+  declarations: [CopyrightComponent, OrHrComponent],
+  exports: [CopyrightComponent, OrHrComponent],
   imports: [CommonModule, TranslateModule],
 })
 export class SharedModule {}
