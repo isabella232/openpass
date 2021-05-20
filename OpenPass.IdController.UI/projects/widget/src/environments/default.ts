780afd95ec1c83a4124959e85d071d5410d116f0
Dmytro <d.kovtun@criteo.com>
2021-05-20 15:34:30 +0300

    Give the ability for users to opt-out from the open SSO
    Change-Id: I3c2c86894a22653a2a15b160eb4ac95c621292e9
    JIRA:USRF-72

--- OpenPass.IdController.UI/projects/widget/src/environments/default.ts
+++ OpenPass.IdController.UI/projects/widget/src/environments/default.ts
@@ -1,6 +1,7 @@
 export const defaultEnv = {
   cookieUid2Token: '__uid2_advertising_token',
   cookieIfaToken: '__ifa',
+  cookieOptoutFlag: '__optout',
   cookieLifetimeDays: 390,
   unloggedPath: '/unauthenticated',
 };
