f408b10b4f901f658b98aa9543655966b3b98904
Dmytro <d.kovtun@criteo.com>
2021-06-04 17:26:37 +0300

    [OpenPass] Provide an additional data to the tracking event
    Change-Id: I2277c3dd55c96aaeab2b924d65ebc046b915c683
    JIRA:USRF-97

--- OpenPass.IdController.UI/projects/widget/src/app/rest/event-tracking/event-tracking.service.ts
+++ OpenPass.IdController.UI/projects/widget/src/app/rest/event-tracking/event-tracking.service.ts
@@ -4,19 +4,36 @@ import { EventTypes } from '@shared/enums/event-types.enum';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { WINDOW } from '../../utils/injection-tokens';
+import { WidgetConfigurationService } from '../../services/widget-configuration.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventTrackingService {
-  constructor(private http: HttpClient, @Inject(WINDOW) private window: Window) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(WINDOW) private window: Window,
+    private widgetConfigurationService: WidgetConfigurationService
+  ) {}
 
   track(eventType: EventTypes): Observable<void> {
-    const originHost = this.window.location.origin;
-    return this.http.post<void>(
-      environment.appHost + '/api/event',
-      { eventType },
-      { headers: { 'x-origin-host': originHost } }
+    return this.widgetConfigurationService.getConfiguration().pipe(
+      mergeMap((config) => {
+        const originHost = this.window.location.origin;
+        const trackedData = {
+          originHost,
+          view: config.view,
+          variant: config.variant,
+          session: config.session,
+          provider: config.provider,
+        };
+        return this.http.post<void>(
+          environment.appHost + '/api/event',
+          { eventType },
+          { headers: { 'x-tracked-data': JSON.stringify(trackedData) } }
+        );
+      })
     );
   }
 }
